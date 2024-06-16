b09735069da61d9c34cafcf98355cfd8af49f184
RayaRPARepo <mohamedabdelnabysaleh97@gmail.com>
2024-06-16 16:01:08 +0300

    Active Directory and powershell sequences added

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace RayaRPARepository
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
